265c2f1964b8ab033e4d498f64b03a9873abd43f
Chrisjs28 <christopher.curran@sky.uk>
2017-04-14 23:27:00 +0100

    set up for recycler view not fully working but half way there

--- app/src/main/java/com/example/android/giphyapi/fragments/RecentFragment.java
+++ app/src/main/java/com/example/android/giphyapi/fragments/RecentFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,9 +22,13 @@ public class RecentFragment extends ShareableFragment {
 
     private static final String TAG = "RecyclerViewFragment";
     private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
 
     @BindView(R.id.recent_searches)
     CardView cardView;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
 
     private enum LayoutManagerType {
         GRID_LAYOUT_MANAGER,
@@ -30,8 +36,8 @@ public class RecentFragment extends ShareableFragment {
     }
 
     protected LayoutManagerType currentLayoutManagerType;
-    protected RecyclerView recyclerView;
     protected RecyclerView.LayoutManager layoutManager;
+    protected String[] dataset;
 
     //https://developer.android.com/samples/RecyclerView/src/com.example.android.recyclerview/RecyclerViewFragment.html
 
@@ -51,6 +57,15 @@ public class RecentFragment extends ShareableFragment {
         View v = inflater.inflate(R.layout.fragment_recent, container, false);
         ButterKnife.bind(this, v);
         cardView.setCardBackgroundColor(getResources().getColor(R.color.colorAccent));
+        layoutManager = new LinearLayoutManager(getActivity());
+        currentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            currentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+        setRecyclerViewLayoutManager(currentLayoutManagerType);
         return v;
     }
 
@@ -67,5 +82,39 @@ public class RecentFragment extends ShareableFragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save currently selected layout manager.
+        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, currentLayoutManagerType);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (recyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) recyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                layoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
+                currentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                layoutManager = new LinearLayoutManager(getActivity());
+                currentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                layoutManager = new LinearLayoutManager(getActivity());
+                currentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.scrollToPosition(scrollPosition);
     }
 }
