898758377b47ed4b44a8de8c1b5ac4caa5bb783b
Chrisjs28 <christopher.curran@sky.uk>
2017-04-14 23:07:10 +0100

    start of recycler view fragement

--- app/src/main/java/com/example/android/giphyapi/fragments/RecentFragment.java
+++ app/src/main/java/com/example/android/giphyapi/fragments/RecentFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +18,20 @@ import butterknife.ButterKnife;
 
 public class RecentFragment extends ShareableFragment {
 
+    private static final String TAG = "RecyclerViewFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+
     @BindView(R.id.recent_searches)
     CardView cardView;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    protected LayoutManagerType currentLayoutManagerType;
+    protected RecyclerView recyclerView;
+    protected RecyclerView.LayoutManager layoutManager;
 
     //https://developer.android.com/samples/RecyclerView/src/com.example.android.recyclerview/RecyclerViewFragment.html
 
