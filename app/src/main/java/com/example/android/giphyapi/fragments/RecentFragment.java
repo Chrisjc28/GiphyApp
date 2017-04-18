9db214c339870238dc9aaf717960c1e46cdaefdb
Chrisjs28 <christopher.curran@sky.uk>
2017-04-18 09:01:25 +0100

    adpater is now being set, need to add data to the recycle view

--- app/src/main/java/com/example/android/giphyapi/fragments/RecentFragment.java
+++ app/src/main/java/com/example/android/giphyapi/fragments/RecentFragment.java
@@ -7,12 +7,14 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.giphyapi.R;
 import com.example.android.giphyapi.activity.ShareableFragment;
+import com.example.android.giphyapi.adapters.CustomAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,20 +25,21 @@ public class RecentFragment extends ShareableFragment {
     private static final String TAG = "RecyclerViewFragment";
     private static final String KEY_LAYOUT_MANAGER = "layoutManager";
     private static final int SPAN_COUNT = 2;
-
-    @BindView(R.id.recent_searches)
-    CardView cardView;
-
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
+    private static final int DATASET_COUNT = 60;
 
     private enum LayoutManagerType {
         GRID_LAYOUT_MANAGER,
         LINEAR_LAYOUT_MANAGER
     }
 
+    @BindView(R.id.recent_searches)
+    CardView cardView;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
     protected LayoutManagerType currentLayoutManagerType;
     protected RecyclerView.LayoutManager layoutManager;
+    protected CustomAdapter mAdapter;
     protected String[] dataset;
 
     //https://developer.android.com/samples/RecyclerView/src/com.example.android.recyclerview/RecyclerViewFragment.html
@@ -72,6 +75,26 @@ public class RecentFragment extends ShareableFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
+        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
+        // elements are laid out.
+        layoutManager = new LinearLayoutManager(getActivity());
+
+        currentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            currentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+        setRecyclerViewLayoutManager(currentLayoutManagerType);
+
+        mAdapter = new CustomAdapter(dataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        recyclerView.setAdapter(mAdapter);
+
+        Log.i(TAG, "onViewCreated: Adapter set");
+
     }
 
     @Override
